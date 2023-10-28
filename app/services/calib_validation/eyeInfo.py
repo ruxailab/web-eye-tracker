d51ab7b7bca879f4faab2af14c68a1c61e7f297a
Natanco <nata.oliveira2004@gmail.com>
2023-10-28 17:28:56 -0300

    refactor(calib): major refactor and code clean up

--- app/services/calib_validation/eyeInfo.py
+++ app/services/calib_validation/eyeInfo.py
@@ -1,35 +1,32 @@
 import random
-import csv
 import tkinter as tk
+import pandas as pd
 
 class EyeInfo:
-    def __init__(self, right_eye =[], left_eye=[], prediction=[], calib_points=[], dataset='./data.csv', screen_width=0, screen_height=0, k_screen_width = 1872, k_screen_height = 944, right = True, left = True):
-            self.right_eye = right_eye
-            self.left_eye = left_eye
-            self.prediction = prediction
-            self.calib_points = calib_points
+    def __init__(self,  calib_points=[], dataset='./data.csv', screen_width=0, screen_height=0, k_screen_width = 1872, k_screen_height = 944, is_right = True, is_left = True):
+            self.is_right = is_right
+            self.is_left = is_left
             self.dataset = dataset
+
+            self.right_eye_df = None
+            self.left_eye_df = None
+            self.prediction_df = None
+
+            self.calib_points = calib_points
+            self.calib_df = None
+
+            
+
             self.screen_width = screen_width
             self.screen_height = screen_height
             self.k_screen_width = k_screen_width
             self.k_screen_height = k_screen_height
-            self.right = right
-            self.left = left
 
-    def init_calib_points(self):
-        if self.calib_points:
-            self.init_screen_resolution()
-            # m_factorx = self.screen_width/self.k_screen_width
-            # m_factory = self.screen_height/self.k_screen_height
-            post_calib = []
-            for point in self.calib_points:
-                calibrated_point = {
-                    "x": point["x"],
-                    "y": point["y"]
-                }
-                post_calib.append(calibrated_point)
-
-            self.calib_points = post_calib
+    def init_eye(self):
+        self.init_screen_resolution()
+        self.init_calib_points()
+        self.init_points()
+        
 
     def init_screen_resolution(self):
         root = tk.Tk()
@@ -38,69 +35,32 @@ class EyeInfo:
         self.screen_height = root.winfo_screenheight()
         root.destroy()
 
+    def init_calib_points(self):
+        if self.calib_points:
+            post_calib = []
+            for point in self.calib_points:
+                calibrated_point = {
+                    "screen_x": point["x"],
+                    "screen_y": point["y"]
+                }
+                post_calib.append(calibrated_point)
+            df = pd.DataFrame(post_calib)
+            self.calib_df = df
+
     def init_points(self):
-        self.init_screen_resolution()
         # m_factorx = self.screen_width/self.k_screen_width
         # m_factory = self.screen_height/self.k_screen_height
         try:
-            with open(self.dataset, 'r', newline='') as file:
-                csv_reader = csv.DictReader(file)
-                for row in csv_reader:
-                    if self.right:
-                        right_eye_point = {
-                            'x': float(row['right_iris_x']),
-                            'y': float(row['right_iris_y'])
-                        }
-                        self.right_eye.append(right_eye_point)
-                    if self.left:
-                        left_eye_point = {
-                            'x': float(row['left_iris_x']),
-                            'y': float(row['left_iris_y'])
-                        }
-                        self.left_eye.append(left_eye_point)
-                        
-                    prediction_point = {
-                        'x':float(row['screen_x']),
-                        'y':float(row['screen_y'])
-                    }
-                    self.prediction.append(prediction_point)
+            data = pd.read_csv(self.dataset)
+            if self.is_right:
+                self.right_eye_df = data[['right_iris_x', 'right_iris_y']]
+            
+            if self.is_left:
+                self.left_eye_df = data[['left_iris_x', 'left_iris_y']]
+
+            self.prediction_df = data[['screen_x', 'screen_y']]
+
         except FileNotFoundError:
             print(f"File {self.dataset} not found.")
         except Exception as e:
             print(f"An error occurred while reading the CSV file: {str(e)}")
-
-    def get_points(self, objArr):
-        x_values = []
-        y_values = []
-        for item in objArr:
-            x_values.append(item['x'])
-            y_values.append(item['y'])
-        return x_values, y_values
-    
-    def get_max_object(self,key):
-        max_object = max(self.prediction, key=lambda obj: obj[f'{key}'])
-        return max_object
-    
-    def get_min_object(self,key):
-        max_object = min(self.prediction, key=lambda obj: obj[f'{key}'])
-        return max_object
-
-    def randomize_points(self, num_objects):
-        objects_right = []
-        objects_left = []
-        objects_prediction = []
-        for _ in range(num_objects):
-            obj = {'x': random.randint(0, 1536), 'y': random.randint(0, 864)}
-            objects_right.append(obj)
-        for _ in range(num_objects):
-            obj = {'x': random.randint(0, 1536), 'y': random.randint(0, 864)}
-            objects_left.append(obj)
-        
-        for _ in range(num_objects):
-            obj = {'x': random.randint(0, 1536), 'y': random.randint(0, 864)}
-            objects_left.append(obj)
-
-        self.right_eye = objects_right
-        self.left_eye = objects_left
-        self.prediction = objects_prediction
-
